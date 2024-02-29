5ae2e6875d234fe9f25c3b7cd7acf9e13368f891
micheltsiresitsilavo <92079707+micheltsiresitsilavo@users.noreply.github.com>
2024-02-29 10:58:18 -0300

    Add files via upload

---
+++ wp/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+   
+    </div>
+    <section class="dark:bg-gray-800 ">
+  <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24 ">
+    <div class="flex flex-col w-full mb-12 text-center">
+      <div class="inline-flex items-center justify-center flex-shrink-0 w-20 h-20 mx-auto mb-5 text-blue-600 rounded-full bg-gray-50">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 icon icon-tabler icon-tabler-aperture" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <circle cx="12" cy="12" r="9"></circle>
+          <line x1="3.6" y1="15" x2="14.15" y2="15"></line>
+          <line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(72 12 12)"></line>
+          <line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(144 12 12)"></line>
+          <line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(216 12 12)"></line>
+          <line x1="3.6" y1="15" x2="14.15" y2="15" transform="rotate(288 12 12)"></line>
+        </svg>
+      </div>
+      <h1 class="max-w-5xl text-2xl font-bold leading-none tracking-tighter dark:text-slate-100 text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
+        Serverless <br class="hidden lg:block">
+       PHP
+      </h1>
+
+      <p class="max-w-xl mx-auto mt-8 text-base leading-relaxed text-center text-gray-500 dark:text-slate-300">Afaka ampiarahina tsara amin'ny resaka Serverless ny PHP. Manamora ny zavatra ataotsika izany raha mitady technique low cost na free isika eo amin'ny hosting.</p>
+
+        <a
+                href="./info.php"
+                class="max-w-xl mx-auto mt-8  rounded-lg border border-blue-700 bg-blue-700 px-7 py-3.5 font-semibold leading-6 text-white hover:border-blue-600 hover:bg-blue-600 hover:text-white focus:ring focus:ring-blue-400 focus:ring-opacity-50 active:border-blue-700 active:bg-blue-700 dark:focus:ring-blue-400 dark:focus:ring-opacity-90"
+              >
+              
+              <svg xmlns="http://www.w3.org/2000/svg" class="hi-mini hi-arrow-down-tray inline-block size-5 opacity-75" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-qr-code"><rect width="5" height="5" x="3" y="3" rx="1"/><rect width="5" height="5" x="16" y="3" rx="1"/><rect width="5" height="5" x="3" y="16" rx="1"/><path d="M21 16h-3a2 2 0 0 0-2 2v3"/><path d="M21 21v.01"/><path d="M12 7v3a2 2 0 0 1-2 2H7"/><path d="M3 12h.01"/><path d="M12 3h.01"/><path d="M12 16v.01"/><path d="M16 12h1"/><path d="M21 12v.01"/><path d="M12 21v-1"/></svg>
+                <span>PHP info</span>
+              </a>
+    </div>
+  </div>
+</section>
+    
+<!-- Features Section: Boxes with Icons -->
+<div
+  class="bg-gradient-to-tr from-indigo-100 via-purple-50 to-teal-100 dark:from-indigo-900 dark:via-purple-900 dark:to-teal-900 dark:text-gray-100"
+>
+  <div class="container mx-auto px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <a
+       href="https://www.php.net/manual/en/refs.database.php"
+        class="rounded-lg border border-white bg-white p-5 shadow-sm transition hover:border-blue-600 dark:border-gray-800 dark:bg-gray-800 dark:shadow-none dark:hover:border-blue-400 md:p-7 xl:p-10 cursor-pointer"
+      >
+      <svg class="hi-outline hi-rectangle-stack mb-5 inline-block size-12 text-blue-600 dark:text-blue-500"   width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-database"><ellipse cx="12" cy="5" rx="9" ry="3"/><path d="M3 5V19A9 3 0 0 0 21 19V5"/><path d="M3 12A9 3 0 0 0 21 12"/></svg>
+        <h4 class="mb-2 text-lg font-bold">1. Database</h4>
+        <p class="leading-relaxed text-gray-600 dark:text-gray-400">
+          Aleo misafidy database hafa ankoatran'ny MySQL, ny antony dia manahirana be amin'tsika eto M/scar ny azahoana azy raha miala amin'ireo website hosting traditionnel isika. Misafidiana amin'ireto: Postgresql, Sqlite, MongoDB. Mora hitatsika ireo.
+        </p>
+      </a>
+      <a
+      href="https://www.redhat.com/en/topics/cloud-native-apps/what-is-serverless"
+        class="rounded-lg border border-white bg-white p-5 shadow-sm transition hover:border-blue-600 dark:border-gray-800 dark:bg-gray-800 dark:shadow-none dark:hover:border-blue-400 md:p-7 xl:p-10 cursor-pointer"
+      >
+        <svg class="hi-outline hi-rectangle-stack mb-5 inline-block size-12 text-blue-600 dark:text-blue-500" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-server-crash"><path d="M6 10H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-2"/><path d="M6 14H4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2"/><path d="M6 6h.01"/><path d="M6 18h.01"/><path d="m13 6-4 6h6l-4 6"/></svg>
+        <h4 class="mb-2 text-lg font-bold">2. Serverless</h4>
+        <p class="leading-relaxed text-gray-600 dark:text-gray-400">
+          Serverless dia mamela ny developers hanamboatra sy handefa application tsy misy fandraisana anjara amin'ny resaka servers. Misy servers ao amin'ny serverless, fa misy abstraction eo amin'ny fampiasana azy.Ireo Cloud Provider no mandray antanana ireo dingana rehetra.
+        </p>
+      </a>
+      <a
+      href="https://www.github.com"
+        class="rounded-lg border border-white bg-white p-5 shadow-sm transition hover:border-blue-600 dark:border-gray-800 dark:bg-gray-800 dark:shadow-none dark:hover:border-blue-400 sm:col-span-2 md:p-7 lg:col-span-1 xl:p-10 cursor-pointer"
+      >
+      <svg class="hi-outline hi-rectangle-stack mb-5 inline-block size-12 text-blue-600 dark:text-blue-500" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+        <h4 class="mb-2 text-lg font-bold">3. Repos on GitHub</h4>
+        <p class="leading-relaxed text-gray-600 dark:text-gray-400">
+         Mipetraka ao amin'ny GitHub ny dossiers misy an'ireo Projet-nao. Afaka mivoatra tsara tsisy hasarotana ny application-nao. Manova dia mi-push...
+        </p>
+      </a>
+    </div>
+  </div>
+</div>
+<!-- END Features Section: Boxes with Icons -->
+
+
+
+</body>
+</html>
+
---
+++ wp/info.php
@@ -0,0 +1,85 @@
+<?php 
+
+$server = $_SERVER['DOCUMENT_ROOT'];
+
+include 'head.php';
+?>
+
+
+
+<body>
+    <div  class="bg-white dark:bg-gray-900 dark:text-gray-100">
+
+        <div
+        class="container relative mx-auto flex flex-col space-y-16 px-4 py-16 text-center lg:flex-row lg:space-y-0 lg:px-8 lg:py-32 lg:text-left xl:max-w-7xl"
+      >
+        <div class="lg:flex lg:w-1/2 lg:items-center">
+          <div>
+            <div
+              class="mb-2 inline-flex rounded border border-gray-200 bg-gray-100 px-2 py-1 text-sm font-medium leading-4 text-gray-800 dark:border-gray-600/50 dark:bg-gray-700/50 dark:text-gray-200"
+            >
+             PHP version <?= phpversion(); ?>
+            </div>
+            <h1 class="mb-4 text-4xl font-black text-black dark:text-white">
+              My server on
+              <span class="text-blue-600 dark:text-blue-500"><?= $server ?></span>
+            </h1>
+            <h2
+              class="text-xl font-medium leading-relaxed text-gray-700 dark:text-gray-300"
+            >
+           Ireo misy information avy any amin'ny php, ahitantsika verion sy ilay serveur misy azy.
+            </h2>
+            <div
+              class="flex flex-col justify-center space-y-2 pb-16 pt-10 sm:flex-row sm:items-center sm:space-x-2 sm:space-y-0 lg:justify-start"
+            >
+              <a
+                href="./index.php"
+                class="inline-flex items-center justify-center space-x-2 rounded-lg border border-blue-700 bg-blue-700 px-7 py-3.5 font-semibold leading-6 text-white hover:border-blue-600 hover:bg-blue-600 hover:text-white focus:ring focus:ring-blue-400 focus:ring-opacity-50 active:border-blue-700 active:bg-blue-700 dark:focus:ring-blue-400 dark:focus:ring-opacity-90"
+              >
+              
+               <svg xmlns="http://www.w3.org/2000/svg" class="hi-mini hi-arrow-down-tray inline-block size-5 opacity-75" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-left"><path d="M6 8L2 12L6 16"/><path d="M2 12H22"/></svg>
+                <span>Go Home</span>
+              </a>
+              <a
+                href="javascript:void(0)"
+                class="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-7 py-3.5 font-semibold leading-6 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700"
+              >
+                <span>Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="lg:ml-16 lg:flex lg:w-1/2 lg:items-center lg:justify-center">
+          <div class="relative mx-5 lg:w-96">
+            <div
+              class="bg-tranparent absolute left-0 top-0 -ml-20 -mt-16 size-40 rounded-full border border-blue-200 dark:border-blue-900 lg:size-72"
+            ></div>
+            <div
+              class="bg-tranparent absolute left-0 top-0 -ml-14 -mt-20 size-40 rounded-full border border-blue-100 dark:border-blue-950 lg:size-72"
+            ></div>
+            <div
+              class="bg-tranparent absolute bottom-0 right-0 -mb-16 -mr-20 size-40 rounded-full border border-blue-200 dark:border-blue-900 lg:size-72"
+            ></div>
+            <div
+              class="bg-tranparent absolute bottom-0 right-0 -mb-20 -mr-14 size-40 rounded-full border border-blue-100 dark:border-blue-950 lg:size-72"
+            ></div>
+            <div
+              class="absolute inset-0 -m-6 -rotate-2 rounded-xl bg-gray-200 dark:bg-gray-800"
+            ></div>
+            <div
+              class="absolute inset-0 -m-6 rotate-1 rounded-xl bg-blue-800 bg-opacity-75 shadow-inner dark:bg-blue-900 dark:bg-opacity-75"
+            ></div>
+            <img
+              src="https://cdn.tailkit.com/media/placeholders/photo-RSCirJ70NDM-800x1000.jpg"
+              class="relative mx-auto rounded-lg shadow-lg"
+              alt="Hero Image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+
+
+
